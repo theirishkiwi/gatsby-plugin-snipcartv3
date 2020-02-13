dd852e6c3ad509c0cfef080ad6b133f9b325a5c0
Dan Holdsworth <daniel.s.holdsworth@gmail.com>
2020-02-13 08:08:47 +0000

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@ var _react2 = _interopRequireDefault(_react);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var warning = false;
-var linkToLangFile = null;
+#var linkToLangFile = null;
 
 exports.onRenderBody = function (_ref) {
 	var setPostBodyComponents = _ref.setPostBodyComponents;
@@ -16,9 +16,9 @@ exports.onRenderBody = function (_ref) {
 	options = Object.assign({
 		apiKey: process.env.GATSBY_SNIPCART_API_KEY,
 		autopop: false,
-		js: 'https://cdn.snipcart.com/themes/v3.0.6/default/snipcart.js',		
-		styles: 'https://cdn.snipcart.com/themes/v3.0.6/default/snipcart.css',
-		language: '' + linkToLangFile
+		js: 'https://cdn.snipcart.com/themes/v3.0.7/default/snipcart.js',		
+		styles: 'https://cdn.snipcart.com/themes/v3.0.7/default/snipcart.css',
+		#language: '' + linkToLangFile
 	}, options);
 
 	if (!options.apiKey) {
