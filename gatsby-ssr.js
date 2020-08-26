a69061623269e43da1670a8dc292295cd0923f32
theirishkiwi <theirishkiwi@gmail.com>
2020-08-26 10:24:29 +0100

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,10 +10,7 @@ var warning = false;
 
 exports.onRenderBody = function (_ref) {
   const _options = {
-    ...{
       version: "3.0.20",
-    },
-    ...pluginOptions,
   };
 	var setPostBodyComponents = _ref.setPostBodyComponents;
 	var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
