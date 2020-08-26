cbbb754ebe64fa378e49c25191abb8dc31862f5a
theirishkiwi <theirishkiwi@gmail.com>
2020-08-26 14:59:34 +0100

    comment store

--- components/SnipcartProvider.js
+++ components/SnipcartProvider.js
@@ -5,18 +5,18 @@ exports.default = void 0;
 
 var React = require('react');
 
-var _require = require('../store'),
-    useStore = _require.useStore,
-    SnipcartContext = _require.SnipcartContext;
+//var _require = require('../store'),
+//    useStore = _require.useStore,
+//    SnipcartContext = _require.SnipcartContext;
 /**
  * @param props : {currency, version}
  */
 
 
-var SnipcartProvider = function SnipcartProvider(props) {
-  var _useStore = useStore(),
-      state = _useStore[0],
-      dispatch = _useStore[1];
+//var SnipcartProvider = function SnipcartProvider(props) {
+//  var _useStore = useStore(),
+//      state = _useStore[0],
+//      dispatch = _useStore[1];
 
   var defaultLang = props.defaultLang,
       locales = props.locales;
